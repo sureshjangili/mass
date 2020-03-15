75d7bd585a0e84abb36ad2cd6eedbc89db55570c
suresbabu <sureshjangili123@gmail.com>
2020-03-15 19:36:57 +0530

    commited

---
+++ SB-HelloWorld1/src/main/java/com/nt/SbHelloWorld1Application.java
@@ -0,0 +1,15 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbHelloWorld1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbHelloWorld1Application.class, args);
+	
+		System.out.println("Welcome to Sb");
+	}
+
+}
